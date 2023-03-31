19deddf313a7d05d493537e9287370aa0dd62ecd
thaynasantosss <thayna.2015.09@gmail.com>
2023-03-31 20:26:55 -0300

    Projeto concluído

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("*** Imitando Dory ***\n");
+
+Console.Write(" diga uma frase para imitar a Dory: ");
+string frase = Console.ReadLine()!;
+
+string fraseEmBaleies = frase
+    .Replace("a", "aaa")
+    .Replace("á", "ááá")
+    .Replace("Á", "ÁÁÁ")
+    .Replace("A", "AAA")
+    .Replace("e", "eee")
+    .Replace("é", "ééé")
+    .Replace("E", "EEE")
+    .Replace("i", "iii")
+    .Replace("I", "III")
+    .Replace("o", "ooo")
+    .Replace("O", "OOO")
+    .Replace("u", "uuu")
+    .Replace("U", "UUU")
+    .Replace("É", "ÉÉÉ")
+;
+
+Console.WriteLine($"\nEm baleiês: {fraseEmBaleies}");
